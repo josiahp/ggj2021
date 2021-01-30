2b3ab9dce5b4b5a17692d95826f4411bf10048eb
Josiah Purtlebaugh <josiah.purtlebaugh@gmail.com>
2021-01-31 01:19:28 +0900

    add voice notes ui

---
+++ Assets/Scripts/VoiceNoteController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceNoteController : MonoBehaviour
+{
+    public GameObject blueVoiceNoteContainer;
+    private List<GameObject> blueVoiceNotes;
+    private int voice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blueVoiceNotes = new List<GameObject>();
+        for (int i = 0; i < blueVoiceNoteContainer.transform.childCount; i++) {
+            blueVoiceNotes.Add(blueVoiceNoteContainer.transform.GetChild(i).gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateVoiceIcons()
+    {
+        for (int i = 0; i < blueVoiceNotes.Count; i++)
+        {
+            if (voice <= i)
+            {
+                blueVoiceNotes[i].SetActive(true);
+            }
+            else
+            {
+                blueVoiceNotes[i].SetActive(false);
+            }
+        }
+    }
+}
