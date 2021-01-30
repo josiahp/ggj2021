15c7034e3c79b8fc20da2bf0ba0d3824bbdb39f0
Josiah Purtlebaugh <josiah.purtlebaugh@gmail.com>
2021-01-30 18:25:50 +0900

    add new echolocator sprite, make it rotate and track

--- Assets/Scripts/Echolocator.cs
+++ Assets/Scripts/Echolocator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Echolocator : MonoBehaviour
-{   
+{
     public float Distance;
     public float fireRate = 0.2f;
     float timeUntilFire;
@@ -14,35 +14,54 @@ public class Echolocator : MonoBehaviour
 
     private Transform cam;
 
-    public void Start() {
+    public GameObject ping;
+    public SpriteRenderer pingSprite;
+    public float pingDuration = 0.75f;
+    private float pingTimeRemaining;
+
+    public void Start()
+    {
         cam = Camera.main.transform;
+        pingSprite = ping.GetComponent<SpriteRenderer>();
+        ping.SetActive(false);
     }
-    
-    private void Update() {
-        //  && timeUntilFire < Time.time
-        if (Input.GetMouseButtonDown(0)) {
+
+    private void Update()
+    {
+        if (pingTimeRemaining <= 0 && Input.GetMouseButtonDown(0))
+        {
             locate();
-            //timeUntilFire = Time.time + fireRate;
+        }
+
+        if (pingTimeRemaining <= 0) {
+            ping.SetActive(false);
+        } else {
+            pingSprite.color = new Color(1f, 1f, 1f, easeOutCubic(pingTimeRemaining / pingDuration));
+            pingTimeRemaining -= Time.deltaTime;
         }
     }
-    public void locate() {
+    public void locate()
+    {
+        ping.SetActive(true);
+        pingTimeRemaining = pingDuration;
+
         GameObject[] NPCs = GameObject.FindGameObjectsWithTag(lookFor);
-        //Debug.Log(lookFor);
-        //Debug.Log(NPCs);
-        foreach (GameObject NPC in NPCs) {
-            //Debug.Log(NPC.name);
-            //Debug.Log(Vector3.Distance(this.transform.position, NPC.transform.position));
-            if (Vector3.Distance(this.transform.position, NPC.transform.position) < Distance) {
+        foreach (GameObject NPC in NPCs)
+        {
+            if (Vector3.Distance(this.transform.position, NPC.transform.position) < Distance)
+            {
                 PingBack(NPC);
             }
         }
     }
 
-    public void PingBack(GameObject NPC) {
-        Vector2 Direction = new Vector2 (this.transform.position.x - NPC.transform.position.x, this.transform.position.y - NPC.transform.position.y).normalized;
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(NPC.transform.position.x, NPC.transform.position.y), Direction, Mathf.Infinity, layer);
-        //Debug.Log(hit.transform.position.x);
-        //Debug.Log(hit.transform.position.y);
-        Instantiate(locator, hit.point, Quaternion.Euler(new Vector3(0f, 0f, 0f)));
+    public void PingBack(GameObject NPC)
+    {
+        GameObject ping = Instantiate(locator, NPC.transform.position, Quaternion.Euler(new Vector3(0f, 0f, 0f)));
+        ping.transform.parent = NPC.transform;
+    }
+
+    float easeOutCubic(float i) {
+        return 1 - Mathf.Pow(1 - i, 3);
     }
 }
